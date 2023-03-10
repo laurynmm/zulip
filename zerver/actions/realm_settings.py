042b488996a1e7bf73284f3ef7f1dc1305f8c6d6
Lauryn Menard <lauryn@zulip.com>
2023-03-10 16:46:13 +0100

    corporate: Send notification when realm plan type changed.
    Updates `do_change_realm_plan_type` to send a notification message
    to the settings.SYSTEM_BOT_REALM if there is a stream in that realm
    named "signups".
    
    The base message contains the organization's name with a link to
    its admin realm support page, as well as the organization type,
    the number of users in the realm and the names of the old and new
    plans.
    
    Because the realm plan type is changed from self-hosted to limited
    when an new organization is registered and settings.BILLING_ENABLED
    is True, we check to make sure any users are in the realm before
    sending the notification. The admin realm already receives a
    notification message when a new organization is registered.

--- zerver/actions/realm_settings.py
+++ zerver/actions/realm_settings.py
@@ -6,18 +6,22 @@ from django.conf import settings
 from django.db import transaction
 from django.db.models import QuerySet
 from django.utils.timezone import now as timezone_now
+from django.utils.translation import gettext as _
+from django.utils.translation import override as override_language
 
 from confirmation.models import Confirmation, create_confirmation_link, generate_key
 from zerver.actions.custom_profile_fields import do_remove_realm_custom_profile_fields
 from zerver.actions.message_delete import do_delete_messages_by_sender
+from zerver.actions.message_send import internal_send_stream_message
 from zerver.actions.user_groups import update_users_in_full_members_system_group
 from zerver.actions.user_settings import do_delete_avatar_image
 from zerver.lib.message import parse_message_time_limit_setting, update_first_visible_message_id
 from zerver.lib.retention import move_messages_to_archive
 from zerver.lib.send_email import FromAddress, send_email_to_admins
 from zerver.lib.sessions import delete_user_sessions
+from zerver.lib.streams import get_signups_stream
 from zerver.lib.upload import delete_message_attachments
-from zerver.lib.user_counts import realm_user_count_by_role
+from zerver.lib.user_counts import realm_user_count, realm_user_count_by_role
 from zerver.models import (
     ArchivedAttachment,
     Attachment,
@@ -34,12 +38,15 @@ from zerver.models import (
     UserGroup,
     UserProfile,
     active_user_ids,
+    get_org_type_display_name,
     get_realm,
+    get_system_bot,
 )
 from zerver.tornado.django_api import send_event, send_event_on_commit
 
 if settings.BILLING_ENABLED:
     from corporate.lib.stripe import downgrade_now_without_creating_additional_invoices
+    from corporate.lib.support import get_plan_name, get_support_url
 
 
 def active_humans_in_realm(realm: Realm) -> QuerySet[UserProfile]:
@@ -516,6 +523,50 @@ def do_change_realm_plan_type(
             "upload_quota_gb",
         ]
     )
+
+    # Send a notification to the admin realm when an organization changes it's plan type.
+    if settings.BILLING_ENABLED:
+        admin_realm = get_realm(settings.SYSTEM_BOT_REALM)
+        sender = get_system_bot(settings.NOTIFICATION_BOT, admin_realm.id)
+
+        user_count = realm_user_count(realm)
+
+        # When an organization is registered on Zulip Cloud the default plan type
+        # is changed from self-hosted to limited before the first user is registered.
+        # We only want to send this notification message for subsequent changes to
+        # the organization's plan type.
+        if user_count > 0:
+            support_url = get_support_url(realm)
+            organization_type = get_org_type_display_name(realm.org_type)
+            old_plan_name = get_plan_name(old_value)
+            new_plan_name = get_plan_name(plan_type)
+
+            with override_language(admin_realm.default_language):
+                message = _(
+                    "[{realm_name}]({support_link}) ({type}) with {user_count} users has changed from {old_plan} to {new_plan}"
+                ).format(
+                    realm_name=realm.display_subdomain,
+                    support_link=support_url,
+                    type=organization_type,
+                    user_count=user_count,
+                    old_plan=old_plan_name,
+                    new_plan=new_plan_name,
+                )
+                topic = _("{realm}: plan change").format(realm=realm.display_subdomain)
+
+            try:
+                signups_stream = get_signups_stream(admin_realm)
+
+                internal_send_stream_message(
+                    sender,
+                    signups_stream,
+                    topic,
+                    message,
+                )
+            except Stream.DoesNotExist:  # nocoverage
+                # If the signups stream hasn't been created in the admin
+                # realm, don't auto-create it to send to it; just do nothing.
+                pass
 
     event = {
         "type": "realm",
